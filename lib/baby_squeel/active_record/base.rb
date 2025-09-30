77ec7b85e97eab82d278deb06149a8502cf7e49a
Jonas S <jonas_s@posteo.de>
2025-09-30 11:29:06 +0200

    remove sifters

--- lib/baby_squeel/active_record/base.rb
+++ lib/baby_squeel/active_record/base.rb
@@ -16,23 +16,6 @@ module BabySqueel
                :minimizing,
                :summing,
                to: :all
-
-      # Define a sifter that can be used within DSL blocks.
-      #
-      # ==== Examples
-      #    class Post < ActiveRecord::Base
-      #      sifter :name_contains do |string|
-      #        name =~ "%#{string}%"
-      #      end
-      #    end
-      #
-      #    Post.where.has { sift(:name_contains, 'joe') }
-      #
-      def sifter(name, &block)
-        define_singleton_method "sift_#{name}" do |*args|
-          DSL.evaluate_sifter(self, *args, &block)
-        end
-      end
     end
   end
 end
